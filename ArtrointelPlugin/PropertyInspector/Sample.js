b904d3d4732d779b8c31f31f252a48ba61687e48
artrointel <artrointel@gmail.com>
2021-06-14 08:51:46 +0900

    Initial update for Avengers key plugin
    Updated SamplePlugin to ArtrointelPlugin.
    Updated Property Inspector UI.

---
+++ ArtrointelPlugin/PropertyInspector/Sample.js
@@ -0,0 +1,13 @@
+﻿function resetCounter() {
+    var payload = {};
+    payload.property_inspector = 'resetCounter';
+    sendPayloadToPlugin(payload);
+}
+
+function onBtnFunctionEditClicked() {
+    var result = window.open('functionPI/functionPI.html', 'Avengers Function Configuration');
+}
+
+function onBtnEffectEditClicked() {
+    var result = window.open('effectPI/effectPI.html', 'Avengers Function Configuration');
+}
---
+++ ArtrointelPlugin/PropertyInspector/effectPI/effectPI.js
@@ -0,0 +1,35 @@
+﻿function resetCounter() {
+    var payload = {};
+    payload.property_inspector = 'resetCounter';
+    sendPayloadToPlugin(payload);
+}
+
+function onBtnCancelClicked() {
+    if (confirm('This action will abort changes if you edited something in this page.')) {
+        window.close();
+    }
+}
+
+function onBtnApplyClicked() {
+    // process saving all changes
+}
+var id = 1;
+function onAddNewEvent() {
+    var eventList = document.getElementById('dvEffectList');
+	eventList.innerHTML +=
+		`<div class="sdpi-item" id="dEvent${id}">
+			<select class="sdpi-item-value" id="sTrigger${id}">
+				<option value="onKeyPressed">OnKeyPressed</option>
+				<option value="onKeyReleased">OnKeyReleased</option>
+			</select>
+			<select class="sdpi-item-value" id="sFunction${id}">
+				<option value="fOpenWebPage">Image Update</option>
+				<option value="fOpenWebPage">Flash</option>
+				<option value="fExecuteProgram">Splash Circle</option>
+				<option value="fShortcutKey">Pie</option>
+				<option value="fExecuteProgram">Border Wave</option>
+			</select>
+			<button class="sdpi-item-value" id="btnConfiguration${id}">Edit Config</button>
+		</div>`;
+	id++;
+}
