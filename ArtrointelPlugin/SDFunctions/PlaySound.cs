44a27aeadbd3c261c6e649691ffe399360dae1e6
artrointel <artrointel@gmail.com>
2021-07-07 19:16:25 +0900

    updated PlaySound function to use NAudio library

--- ArtrointelPlugin/SDFunctions/PlaySound.cs
+++ ArtrointelPlugin/SDFunctions/PlaySound.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Windows.Media;
+using System.Threading;
+using System.Threading.Tasks;
+using NAudio.Wave;
 using ArtrointelPlugin.Utils;
 
+// https://github.com/naudio/NAudio/blob/master/Docs/PlayAudioFileConsoleApp.md
 namespace ArtrointelPlugin.SDFunctions
 {
+    // TODO In 2.0 Update, playsound will have more detail options with toggle key feature;
+    // volume, loop, pause/stop/restart
     internal class PlaySound : FunctionBase
     {
-        private double mVolume = 1.0;
-        private string mFilePath;
-
+        
         internal PlaySound(string metadata)
             : base(metadata)
         {
@@ -17,17 +20,31 @@ namespace ArtrointelPlugin.SDFunctions
 
         public override void execute(bool restart)
         {
-            if(restart)
+            if (restart)
             {
-                var player = new MediaPlayer();
-                player.Open(new Uri(mFilePath));
-                player.Volume = mVolume;
-                player.Play();
-                // TODO call Close(). and use mPlayer.Dispatcher for threading
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        using (var audioFile = new AudioFileReader(mMetadata))
+                        using (var outputDevice = new WaveOutEvent())
+                        {
+                            outputDevice.Init(audioFile);
+                            outputDevice.Play();
+                            while (outputDevice.PlaybackState == PlaybackState.Playing)
+                            {
+                                Thread.Sleep(1000);
+                            }
+                        }
+                    } catch (Exception e)
+                    {
+                        DLogger.LogMessage("Cannot play file :" + mMetadata + ", "+ e.Message);
+                    }
+                });
             }
             else
             {
-                
+
             }
         }
     }
